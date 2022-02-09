401a949816c9a3d2c682cb9ef4155213e147fb09
Timothy Dorman <94666851+TCDorman@users.noreply.github.com>
2022-02-08 22:26:51 -0500

    created the polite reminder and added the text to follow

--- src/reminder.py
+++ src/reminder.py
@@ -6,11 +6,12 @@ class PrefixedReminder:
         self.prefix = prefix
         self.text = prefix + '<placeholder_text>'
 
-class PoliteReminder:
+class PoliteReminder(PrefixedReminder):
     """Tis class inerits from Prefixed Reminder. Its __init__() should 
     accept a text parameter; for now you don't need to use it. Initiate 
     the parent class by calling super().__init__() with a polite prefix 
     (the prefix should contain the word "please").
     """
-    def __init__(text, prefix="please"):
-        super().__init__()
+    def __init__(self, text):
+        super().__init__(prefix="please remember to ")
+        self.text = self.prefix + text
