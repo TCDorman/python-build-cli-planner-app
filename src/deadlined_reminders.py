30a5a3ea76d3ffc7bdf1212cbf8415bacb769e0f
Timothy Dorman <94666851+TCDorman@users.noreply.github.com>
2022-02-08 23:16:06 -0500

    a whole lot'

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -1,6 +1,7 @@
 from abc import ABC, ABCMeta, abstractmethod
 from collections.abc import Iterable
 from datetime import date
+from attr import attr
 from dateutil.parser import parse
 from datetime import datetime
 
@@ -19,6 +20,19 @@ class DeadlinedReminder(Iterable, ABC):
     @abstractmethod
     def is_due(self):
         pass
+
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        if cls is not DeadlinedReminder:
+            return NotImplemented
+
+        def attr_in_hierarchy(attr):
+            return any (attr in SuperClass.__dict__ for SuperClass in subclass.__mro__)
+
+        if not all (attr_in_hierarchy(attr) for attr in ('__iter__', 'is_due')):
+            return NotImplemented
+
+        return True
 
 
 class DateReminder(DeadlinedReminder):
