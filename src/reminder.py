24cc7e402c84db9b098ed262e37611e3c6421689
Timothy Dorman <94666851+TCDorman@users.noreply.github.com>
2022-02-08 22:16:18 -0500

    deleted extra file and created PoliteReminder Class

--- src/reminder.py
+++ src/reminder.py
@@ -5,3 +5,12 @@ class PrefixedReminder:
     def __init__(self, prefix="Hey, don't forget to "):
         self.prefix = prefix
         self.text = prefix + '<placeholder_text>'
+
+class PoliteReminder:
+    """Tis class inerits from Prefixed Reminder. Its __init__() should 
+    accept a text parameter; for now you don't need to use it. Initiate 
+    the parent class by calling super().__init__() with a polite prefix 
+    (the prefix should contain the word "please").
+    """
+    def __init__(text, prefix="please"):
+        super().__init__()
